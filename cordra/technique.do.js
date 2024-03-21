86d25875e0148cb58475b698797f51b1328a9454
kobbejager <wfremout+github@gmail.com>
2024-03-21 16:23:12 +0100

    add vocab terms and preferred label to technique

--- cordra/technique.do.js
+++ cordra/technique.do.js
@@ -5,5 +5,18 @@ exports.beforeSchemaValidation = beforeSchemaValidation;
 
 function beforeSchemaValidation(object, context) {
     object.content.$schema = schema.$id;
+    
+    // get the preferred label for the technique
+    const controlledListSchema = require('/cordra/schemas/Controlled lists.schema.json');
+    const cl_enum = controlledListSchema.definitions.technique.enum;
+    const cl_titles = controlledListSchema.definitions.technique.options.enum_titles;
+
+    let index = cl_enum.indexOf(object.content.e-rihs_vocab_id);
+    if (index > -1) {
+        object.content.preferred_label = cl_titles[index];
+    } else {
+        object.content.preferred_label = "[untitled technique]";
+    } 
+
     return object;
 }
