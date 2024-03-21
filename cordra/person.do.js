09fd2801b9b14143fe328ebba2bc4deca8cd33e5
kobbejager <wfremout+github@gmail.com>
2024-03-21 15:31:32 +0100

    fix person's full name

--- cordra/person.do.js
+++ cordra/person.do.js
@@ -5,5 +5,6 @@ exports.beforeSchemaValidation = beforeSchemaValidation;
 
 function beforeSchemaValidation(object, context) {
     object.content.$schema = schema.$id;
+    object.content.full_name = object.content.last_name + ', ' + object.content.first_name;
     return object;
 }
