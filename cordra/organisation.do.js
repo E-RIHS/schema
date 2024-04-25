4fcb63c1b80dae4c8b350e9866fb8d290faff256
kobbejager <wfremout+github@gmail.com>
2024-04-25 16:59:01 +0200

    add hidden display name in organisation

--- cordra/organisation.do.js
+++ cordra/organisation.do.js
@@ -5,5 +5,6 @@ exports.beforeSchemaValidation = beforeSchemaValidation;
 
 function beforeSchemaValidation(object, context) {
     object.content.$schema = schema.$id;
+    object.content.display_name = object.content.name + ' (' + object.content.acronym + ')';
     return object;
 }
